582061a7939e7fa5e2a3e1b1b1b84c289c6e0673
PashayanEster <159801831+PashayanEster@users.noreply.github.com>
2024-02-23 18:26:18 +0400

    Add files via upload

---
+++ #include <iostream>.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+int max_length(std::string& str){
+    std::string
+    for(int i = 0 ; i < str.size, ++i){
+        for(int j = 0; j < str.size; ++j){
+            if(str[i] == " "){=
+                str[i] = "/0";
+
+            }
+            ++i;
+        }
+    }
+}
